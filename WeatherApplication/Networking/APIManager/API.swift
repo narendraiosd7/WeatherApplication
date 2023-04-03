9b59fcfb294092948dca7cff2ea90e31c22b6e66
nvadde <nvadde@uworld.in>
2023-04-03 19:20:43 +0530

    removed commented code

--- WeatherApplication/Networking/APIManager/API.swift
+++ WeatherApplication/Networking/APIManager/API.swift
@@ -10,7 +10,4 @@ import Foundation
 struct API {
     static let key = "a1aae4f3d544248ff56c732dd4549d1c"
     static let baseURLString = "https://api.openweathermap.org/data/2.5/"
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&amp;appid={API key}
-//https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&amp;appid={API key}
-//https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&amp;appid={API key}
 }
